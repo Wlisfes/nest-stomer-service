303b090cab8c5fcc9baac4cd1271494d1063047c
妖雨纯 <876451336@qq.com>
2023-03-15 19:38:49 +0800

    ✨ feat(entity): 修改用户表解构

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -4,16 +4,14 @@ import { hashSync } from 'bcryptjs'
 
 @Entity('tb-user')
 export class UserEntity extends NEntity {
-	@BeforeInsert()
-	BeforeCreate() {
-		this.uid = Date.now()
-	}
-
-	@Column({ type: 'double', comment: 'uid', readonly: true })
-	uid: number
+	@Column({ type: 'uuid', comment: 'uid', readonly: true })
+	uid: string
 
 	@Column({ charset: 'utf8mb4', comment: '昵称', nullable: false })
 	nickname: string
+
+	@Column({ comment: '邮箱', nullable: true })
+	email: string | null
 
 	@Column({ comment: '头像', nullable: true, default: null })
 	avatar: string
@@ -21,9 +19,11 @@ export class UserEntity extends NEntity {
 	@Column({ comment: 'OpenID', nullable: true, default: null })
 	openid: string
 
+	@Column({ charset: 'utf8mb4', comment: '备注', nullable: true })
+	comment: string | null
+
 	@Column({
 		comment: '手机号',
-		nullable: true,
 		transformer: { from: value => Number(value), to: value => value }
 	})
 	mobile: string
