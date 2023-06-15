072e267e736d3214121cad97b76cca9c44e341d4
妖雨纯 <876451336@qq.com>
2023-06-15 16:56:25 +0800

    ✨ feat(修改首页提示):

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,4 +1,5 @@
 import { Module, Global } from '@nestjs/common'
+import { RouterModule } from '@nestjs/core'
 import { CoreService } from './core.service'
 //module
 import { BasicModule } from '@/module/basic/basic.module'
@@ -9,19 +10,21 @@ import { UserModule } from '@/module/user/user.module'
 import { RouteModule } from '@/module/route/route.module'
 import { RoleModule } from '@/module/role/role.module'
 import { ChacterModule } from '@/module/chacter/chacter.module'
+const modules = [
+	BasicModule,
+	QueueModule,
+	DispatchModule,
+	WeChatModule,
+	UserModule,
+	RouteModule,
+	RoleModule,
+	ChacterModule
+]
+const routes = modules.map(module => ({ path: 'api', module: module }))
 
 @Global()
 @Module({
-	imports: [
-		BasicModule,
-		QueueModule,
-		DispatchModule,
-		WeChatModule,
-		UserModule,
-		RouteModule,
-		RoleModule,
-		ChacterModule
-	],
+	imports: [RouterModule.register(routes), ...modules],
 	providers: [CoreService],
 	exports: [CoreService]
 })
