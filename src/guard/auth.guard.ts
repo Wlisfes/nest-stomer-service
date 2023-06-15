53006f5388de2572bafc1f8f23ef995577fdfa61
妖雨纯 <876451336@qq.com>
2023-06-15 18:02:01 +0800

    ✨ feat(guard): 修改登录守卫

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -9,7 +9,8 @@ export const APP_AUTH_TOKEN = 'x-token'
 
 export class IBearer {
 	decorator: boolean
-	transfer?: boolean
+	transfer: boolean
+	baseURL: string
 }
 
 @Injectable()
@@ -30,8 +31,8 @@ export class AuthGuard implements CanActivate {
 			const token = request.headers[APP_AUTH_TOKEN]
 			if (!token) {
 				//未携带token，未登录
-				if (!bearer.transfer) {
-					//transfer为true时无需抛出错误
+				if (bearer.transfer) {
+					//transfer为true时抛出错误
 					throw new HttpException(i18n.t('user.notice.LOGIN_NOT'), HttpStatus.UNAUTHORIZED)
 				}
 			} else {
@@ -39,8 +40,8 @@ export class AuthGuard implements CanActivate {
 				const cache = await this.redisService.getStore(`user_token_${node.uid}`)
 				if (!cache || cache !== token) {
 					//token未存储在redis中、或者redis中存储的token不一致，登录已过期
-					if (!bearer.transfer) {
-						//transfer为true时无需抛出错误
+					if (bearer.transfer) {
+						//transfer为true时抛出错误
 						throw new HttpException(i18n.t('user.notice.TOKEN_EXPIRE'), HttpStatus.UNAUTHORIZED)
 					}
 				}
