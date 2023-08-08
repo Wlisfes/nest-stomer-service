f2450d9f613aad9ccea0aa5b5140fc3f6727e6bc
妖雨纯 <876451336@qq.com>
2023-08-08 23:54:00 +0800

    ✨ feat(Base): 临时存储

--- src/interceptor/transform.interceptor.ts
+++ src/interceptor/transform.interceptor.ts
@@ -3,19 +3,15 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import * as day from 'dayjs'
 
-interface MapResult<T> {
-	data: T
-}
-
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, MapResult<T>> {
-	async intercept(context: ExecutionContext, next: CallHandler<T>): Promise<Observable<MapResult<T>>> {
+export class TransformInterceptor implements NestInterceptor {
+	async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<unknown>> {
 		return next.handle().pipe(
 			map(data => {
 				return {
 					data: data || null,
 					code: HttpStatus.OK,
-					message: '请求成功',
+					message: data.message ?? '请求成功',
 					timestamp: day().format('YYYY-MM-DD HH:mm:ss')
 				}
 			})
